1e4878dabc2998468f94437f384a0a11178b92aa
aqibyameen <aqibyameen644@gmail.com>
2024-10-10 15:30:35 -0700

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { html } from '../assets'
+import { Html } from '@react-three/drei'
+import { useProgress } from '@react-three/drei'
+
+const Loader = () => {
+  const {progress}= useProgress();
+  return (
+ <Html>
+ <span className="canvas-load">
+  <p style={{
+    fontSize:14,
+    color:"#f1f1f1",
+    fontWeight:800,
+    marginTop:40
+  }}>
+    {progress.toFixed(2)}%
+
+  </p>
+ </span>
+  
+ </Html>
+  )
+}
+
+export default Loader
